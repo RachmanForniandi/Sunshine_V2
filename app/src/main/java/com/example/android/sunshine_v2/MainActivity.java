8fea600b14a31cbed7c492be6aba38809067f40f
RachmanForniandi <rachmanforniandi@gmail.com>
2017-12-04 21:14:51 +0700

    Add networking

--- app/src/main/java/com/example/android/sunshine_v2/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine_v2/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.android.sunshine_v2;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.example.android.sunshine_v2.data.SunshinePreferences;
+import com.example.android.sunshine_v2.utilities.NetworkUtils;
+import com.example.android.sunshine_v2.utilities.OpenWeatherJsonUtils;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,37 +29,45 @@ public class MainActivity extends AppCompatActivity {
         mWeatherTxtView = (TextView)findViewById(R.id.tv_weather_data);
 
 
-        //Create an array of Strings that contain fake weather data
-        /*
-         * This String array contains dummy weather data. Later in the course, we're going to get
-         * real weather data. For now, we want to get something on the screen as quickly as
-         * possible, so we'll display this dummy data.
-         */
-        String[]dummyWeatherData ={
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
+        loadWeatherData();
+    }
 
-        //Append each String from the fake weather data array to the TextView
-        /*
-         * Iterate through the array and append the Strings to the TextView. The reason why we add
-         * the "\n\n\n" after the String is to give visual separation between each String in the
-         * TextView. Later, we'll learn about a better way to display lists of data.
-         */
-        for (String dummyWeatherDay : dummyWeatherData){
-            mWeatherTxtView.append(dummyWeatherDay + "\n\n\n");
+    private void loadWeatherData() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]>{
+        @Override
+        protected String[] doInBackground(String... params) {
+            if (params.length ==0){
+                return null;
+            }
+
+            String location = params[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
+            try {
+                String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+
+                String[]simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+                return simpleJsonWeatherData;
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherData){
+            if (weatherData != null){
+                for (String weatherString : weatherData){
+                    mWeatherTxtView.append((weatherString)+ "\n\n\n");
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/android/sunshine_v2/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/sunshine_v2/utilities/NetworkUtils.java
@@ -1,8 +1,12 @@
 package com.example.android.sunshine_v2.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -31,7 +35,14 @@ public final class NetworkUtils {
     /* The units we want our API to return  */
     private static final String units = "metric";
     /* The number of days we want our API to return */
-    private static String DAYS_PARAM = "cnt";
+    private static final int numDays = 14;
+
+    final static String QUERY_PARAM = "q";
+    final static String LAT_PARAM = "lat";
+    final static String LON_PARAM = "lon";
+    final static String FORMAT_PARAM= "mode";
+    final static String UNITS_PARAM= "units";
+    final static String DAYS_PARAM= "cnt";
 
     /**
      * Builds the URL used to talk to the weather server using a location. This location is based
@@ -41,7 +52,23 @@ public final class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String locationQuery){
-        return null;
+        Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        }catch (MalformedURLException m){
+            m.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI" + url);
+
+        return url;
     }
 
     public static URL buildUrl(Double lat, Double lon){
